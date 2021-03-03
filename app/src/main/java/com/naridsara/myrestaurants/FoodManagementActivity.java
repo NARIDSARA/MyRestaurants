30f2678569e5317bba6fcd190d22f33da9b5b20b
naridsara <bisrychaikong@gmail.com>
2021-03-03 11:40:29 +0700

    set toolbar FoodManagementActivity

--- app/src/main/java/com/naridsara/myrestaurants/FoodManagementActivity.java
+++ app/src/main/java/com/naridsara/myrestaurants/FoodManagementActivity.java
@@ -1,15 +1,22 @@
 package com.naridsara.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class FoodManagementActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_management);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Food management");
+        setSupportActionBar(mToolbar);
     }
 
 }
