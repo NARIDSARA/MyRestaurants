4ea8ed6d4567e493fc0ae164c24de1d34d9569fd
naridsara <bisrychaikong@gmail.com>
2021-03-03 11:38:44 +0700

    create option menu nav to food management

---
+++ app/src/main/java/com/naridsara/myrestaurants/FoodManagementActivity.java
@@ -0,0 +1,15 @@
+package com.naridsara.myrestaurants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_management);
+    }
+
+}
--- app/src/main/java/com/naridsara/myrestaurants/MainActivity.java
+++ app/src/main/java/com/naridsara/myrestaurants/MainActivity.java
@@ -1,12 +1,19 @@
 package com.naridsara.myrestaurants;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.adedom.library.Dru;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,27 @@ public class MainActivity extends AppCompatActivity {
             Dru.failed(getBaseContext());
         } else {
             Dru.completed(getBaseContext());
+        }
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Main");
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.management:
+                startActivity(new Intent(getBaseContext(), FoodManagementActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
