5d1b96bad356abd30a4e8e4c0e008062eb178657
naridsara <bisrychaikong@gmail.com>
2021-03-03 15:21:44 +0700

    add food to server

--- app/src/main/java/com/naridsara/myrestaurants/AddFoodActivity.java
+++ app/src/main/java/com/naridsara/myrestaurants/AddFoodActivity.java
@@ -1,22 +1,28 @@
 package com.naridsara.myrestaurants;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.adedom.library.Dru;
 import com.adedom.library.ExecuteQuery;
+import com.adedom.library.ExecuteUpdate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -27,6 +33,11 @@ public class AddFoodActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private Spinner mSpinner;
     private int foodTypeId;
+    private EditText mEtFoodName;
+    private EditText mEtFoodNameUs;
+    private EditText mEtFoodImage;
+    private EditText mEtFoodPrice;
+    private Button mBtAddFood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,12 @@ public class AddFoodActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("Add food");
         setSupportActionBar(mToolbar);
+
+        mEtFoodName = (EditText) findViewById(R.id.etFoodName);
+        mEtFoodNameUs = (EditText) findViewById(R.id.etFoodNameUs);
+        mEtFoodImage = (EditText) findViewById(R.id.etFoodImage);
+        mEtFoodPrice = (EditText) findViewById(R.id.etFoodPrice);
+        mBtAddFood = (Button) findViewById(R.id.btAddFood);
 
         mSpinner = (Spinner) findViewById(R.id.spinner);
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -51,6 +68,13 @@ public class AddFoodActivity extends AppCompatActivity {
         });
 
         fetchFoodType();
+
+        mBtAddFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogAddFood();
+            }
+        });
 
     }
 
@@ -76,6 +100,49 @@ public class AddFoodActivity extends AppCompatActivity {
                         } catch (SQLException throwables) {
                             throwables.printStackTrace();
                         }
+                    }
+                });
+    }
+
+    private void dialogAddFood() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Add food");
+        dialog.setMessage("Are you sure");
+        dialog.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addFood();
+            }
+        });
+        dialog.show();
+    }
+
+    private void addFood() {
+        String foodName = mEtFoodName.getText().toString().trim();
+        String foodNameUs = mEtFoodNameUs.getText().toString().trim();
+        String foodImage = mEtFoodImage.getText().toString().trim();
+        String foodPrice = mEtFoodPrice.getText().toString().trim();
+
+        if (foodName.isEmpty()) {
+            Toast.makeText(getBaseContext(), "Please enter food name", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String sql = "INSERT INTO `food`(`Food_Name`, `Food_NameUS`, `Food_Image`, `Food_Price`, `Food_Type_ID`, `Created`) " +
+                "VALUES ('" + foodName + "', '" + foodNameUs + "', '" + foodImage + "', " + foodPrice + ", " + foodTypeId + ", CURRENT_TIMESTAMP)";
+        Dru.connection(ConnectDB.getConnection())
+                .execute(sql)
+                .commit(new ExecuteUpdate() {
+                    @Override
+                    public void onComplete() {
+                        finish();
+                        Toast.makeText(getBaseContext(), "Add food success", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
